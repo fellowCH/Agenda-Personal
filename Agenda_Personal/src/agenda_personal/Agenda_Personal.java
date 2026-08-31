10f4a2f6fb6f6a8c476e020f7a3b4a1dbff33123
Ricardo Ortez <Ricardo Ortez@Richard>
2026-08-30 19:50:37 -0600

    Push numero uno 30/8/26. Creando repositorio y pushs.

---
+++ Agenda_Personal/src/agenda_personal/Agenda_Personal.java
@@ -0,0 +1,17 @@
+
+package agenda_personal;
+
+/**
+ *
+ * @author Ricardo Ortez
+ */
+public class Agenda_Personal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
